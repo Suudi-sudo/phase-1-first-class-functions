b8912ae9a8135dc07a36df00168a142714a5c8d0
suudi abdisalan <abdisalansucdi@gmail.com>
2024-09-25 21:04:05 +0300

    complete lab

---
+++ index.js
@@ -0,0 +1,19 @@
+function receivesAFunction(callback){
+    callback();
+}
+function returnsANamedFunction(){
+    function namedFunction(){
+        console.log("I am a named function!");
+    }
+
+    return namedFunction;
+}
+ function returnsAnAnonymousFunction(){
+    return function() {
+        console.log("This is an anonymous function");
+        
+    };
+ }
+
+ const anonymousFunc = returnSAnAnonymousFunction();
+ anonymousFunc();
